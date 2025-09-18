9a2319725d4230c6785e5569ce35c46e3ff828d9
000Shehab000 <shhab200025@gmail.com>
2025-09-18 04:21:13 +0300

    feat: add basic counter UI
    - Created basic UI structure
    - Added counter display
    - Added buttons

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,16 @@
 import './App.css'
 
 export default function App() {
-  return
+  return (
+    <div className="app-container">
+      <div className="counter">Counter :0</div>
+      <div>
+        <button className="btn">Increase +</button>
+        <button className="btn">Decrease -</button>
+      </div>
+      <div>
+        <button className="btn">Hide/Show Counter Number</button>
+      </div>
+    </div>
+  )
 }
