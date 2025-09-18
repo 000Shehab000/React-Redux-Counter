d285cdcd1ee966c4eb2067fed987bff2c4571e36
000Shehab000 <shhab200025@gmail.com>
2025-09-18 21:27:57 +0300

    feat: implement redux counter
    - Implemented increase/decrease
    - Added show/hide functionality
    - Connected to Redux store

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,46 @@
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function App() {
+  const dispatch = useDispatch()
+  const counterState = useSelector((state) => state.counter)
+  const showCounterState = useSelector((state) => state.showCounter)
+
+  const increase = () => {
+    const action = { type: 'increase', payload: { addedNum: 5 } }
+    dispatch(action)
+  }
+  const decrease = () => {
+    const action = { type: 'decrease', payload: { subtractedNum: 1 } }
+    dispatch(action)
+  }
+
+  const showOrHideCounter = () => {
+    const action = { type: 'toggleCounter' }
+    dispatch(action)
+  }
+
   return (
     <div className="app-container">
-      <div className="counter">Counter :0</div>
+      <h1>React Redux Counter</h1>
+      {showCounterState && (
+        <>
+          <div className="counter">Counter :{counterState}</div>
+          <div>
+            <button className="btn" onClick={increase}>
+              Increase +
+            </button>
+            <button className="btn" onClick={decrease}>
+              Decrease -
+            </button>
+          </div>
+        </>
+      )}
+
       <div>
-        <button className="btn">Increase +</button>
-        <button className="btn">Decrease -</button>
-      </div>
-      <div>
-        <button className="btn">Hide/Show Counter Number</button>
+        <button className="btn" onClick={showOrHideCounter}>
+          Hide/Show Counter Number
+        </button>
       </div>
     </div>
   )
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,7 +1,15 @@
 import { createStore } from 'redux'
 
-const initState = { counter: 0 }
+const initState = { counter: 0, showCounter: true }
 const counterReducer = (state = initState, action) => {
+  if (action.type === 'increase') {
+    return { ...state, counter: state.counter + action.payload.addedNum }
+  } else if (action.type === 'decrease') {
+    return { ...state, counter: state.counter - action.payload.subtractedNum }
+  } else if (action.type === 'toggleCounter') {
+    return { ...state, showCounter: !state.showCounter }
+  }
+
   return state
 }
 
