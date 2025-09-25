7de1f9810d9bd8ccc538f273351bc5cd640bf72f
000Shehab000 <shhab200025@gmail.com>
2025-09-25 10:35:54 +0300

    refactor: use Redux Toolkit for state management
    - Migrated to Redux Toolkit
    - Simplified reducer logic

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,18 +1,33 @@
-import { createStore } from 'redux'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 const initState = { counter: 0, showCounter: true }
-const counterReducer = (state = initState, action) => {
-  if (action.type === 'increase') {
-    return { ...state, counter: state.counter + action.payload.addedNum }
-  } else if (action.type === 'decrease') {
-    return { ...state, counter: state.counter - action.payload.subtractedNum }
-  } else if (action.type === 'toggleCounter') {
-    return { ...state, showCounter: !state.showCounter }
-  }
 
-  return state
-}
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initState,
+  reducers: {
+    increase: (state, action) => {
+      state.counter += 1
+    },
+    decrease: (state, action) => {
+      state.counter -= 1
+    },
+  },
+})
 
-const store = createStore(counterReducer)
+// original rudex reducer
+// const counterReducer = (state = initState, action) => {
+//   if (action.type === 'increase') {
+//     return { ...state, counter: state.counter + action.payload.addedNum }
+//   } else if (action.type === 'decrease') {
+//     return { ...state, counter: state.counter - action.payload.subtractedNum }
+//   } else if (action.type === 'toggleCounter') {
+//     return { ...state, showCounter: !state.showCounter }
+//   }
+
+//   return state
+// }
+
+const store = configureStore(counterSlice.reducer)
 
 export default store
