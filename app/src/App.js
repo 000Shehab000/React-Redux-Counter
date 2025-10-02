cf4c6ac0ee6cf1b9402ee0c782dea55920aaebf7
000Shehab000 <shhab200025@gmail.com>
2025-10-02 19:45:47 +0300

    feat(auth): add authentication toggle
    - Added auth state to Redux
    - Added login/logout button

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,35 @@
-import { useEffect, useCallback } from 'react'
 import './App.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { increase, decrease } from './store/counterSlice'
+import { toggle } from './store/authSlice'
 
 export default function App() {
   const dispatch = useDispatch()
   const counterState = useSelector((state) => state.counter.value)
+  const authState = useSelector((state) => state.auth.isLoggedIn)
 
   return (
     <div className="app-container">
       <h1>React Redux Counter</h1>
 
-      <>
-        <div className="counter">Counter : {counterState}</div>
-        <div>
-          <button className="btn" onClick={() => dispatch(increase(5))}>
-            Increase +
-          </button>
-          <button className="btn" onClick={() => dispatch(decrease(2))}>
-            Decrease -
-          </button>
-        </div>
-      </>
+      {authState && (
+        <>
+          <div className="counter">Counter : {counterState}</div>
+          <div>
+            <button className="btn" onClick={() => dispatch(increase(5))}>
+              Increase +
+            </button>
+            <button className="btn" onClick={() => dispatch(decrease(2))}>
+              Decrease -
+            </button>
+          </div>
+        </>
+      )}
 
       <div>
-        <button className="btn">Hide/Show Counter Number</button>
+        <button className="btn" onClick={() => dispatch(toggle())}>
+          {authState ? 'Sign Out' : 'Login'}
+        </button>
       </div>
     </div>
   )
